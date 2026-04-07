b8b6266b5e2410555e0a3be44b2403512f25545e
Thom van de Vin <thom@vandev.in>
2026-04-07 20:15:10 +0200

    fix: add --reset flag to tailscale-autoconnect service
    Tailscale requires --reset when non-default settings (hostname, ssh)
    are already configured, otherwise 'tailscale up' fails and causes
    nixos-rebuild switch to exit non-zero.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -266,7 +266,7 @@
       fi
 
       # otherwise authenticate with tailscale
-      ${tailscale}/bin/tailscale up -authkey $(cat ${config.sops.secrets.tailscale-auth-key.path})
+      ${tailscale}/bin/tailscale up --reset --authkey $(cat ${config.sops.secrets.tailscale-auth-key.path})
     '';
   };
 
